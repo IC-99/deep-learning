5a42060f77b10b8a7707054bc15b23417958e1ea
Ivan Carlini <ivancarlini99@gmail.com>
2024-05-28 19:28:14 +0200

    update

--- create_test_dataset.py
+++ create_test_dataset.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from numpy import random
+from random import randrange
 
 data = {'user_id': [], 'date': [], 'sleepduration': [], 'bedin': [], 'bedout': [], 'awakeduration': [],
         'timetosleep': [], 'remduration': [], 'deepduration': [], 'stepsgaitspeed': [], 'distancegaitspeed': []}
@@ -12,15 +13,23 @@ for user_id in range(1000):
         data['user_id'].append(user_id)
         data['date'].append(date)
 
-        data['sleepduration'].append(get_random_float(7, 4))
-        data['bedin'].append(get_random_float(24, 4))
-        data['bedout'].append(get_random_float(9, 3))
-        data['awakeduration'].append(get_random_float(0.5, 0.25))
-        data['timetosleep'].append(get_random_float(0.5, 0.25))
-        data['remduration'].append(get_random_float(2, 1))
-        data['deepduration'].append(get_random_float(3, 2))
-        data['stepsgaitspeed'].append(get_random_float(95, 15))
-        data['distancegaitspeed'].append(get_random_float(4.5, 1))
+        bedin = get_random_float(24, 1)
+        bedout = get_random_float(9, 1)
+        timetosleep = get_random_float(0.5, 0.1)
+        awakeduration = get_random_float(0.5, 0.1)
+        sleepduration = round(bedout + 24 - bedin - timetosleep - awakeduration, 2)
+        rem_percentage = randrange(15, 30) / 100
+        deep_percentage = randrange(5, 15) / 100
+
+        data['sleepduration'].append(sleepduration)
+        data['bedin'].append(bedin)
+        data['bedout'].append(bedout)
+        data['awakeduration'].append(awakeduration)
+        data['timetosleep'].append(timetosleep)
+        data['remduration'].append(round(sleepduration * rem_percentage, 2))
+        data['deepduration'].append(round(sleepduration * deep_percentage, 2))
+        data['stepsgaitspeed'].append(get_random_float(95, 4))
+        data['distancegaitspeed'].append(get_random_float(4.5, 0.2))
 
 for col in data:
     print(len(data[col]))
